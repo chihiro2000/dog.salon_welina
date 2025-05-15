fb615839c7ef0b596ade5fd1c38389c524cddcc0
chihiro2000 <chihiro.1124.8@icloud.com>
2025-05-15 20:23:19 +0900

    seo accessmap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,13 +2,8 @@
 module.exports = {
   siteUrl: "https://dogsalonwelina.vercel.app", // あなたのサイトのURLに変更
   generateRobotsTxt: true, // robots.txtも生成
-  exclude: ["/admin", "/admin/*"], // クロール対象外のページ
-  robotsTxtOptions: {
-    additionalSitemaps: [
-      // 追加のサイトマップ（動的コンテンツ用などに必要な場合）
-      "https://dogsalonwelina.vercel.app/server-sitemap.xml",
-    ],
-  },
+  exclude: ["/admin", "/admin/*", "/server-sitemap.xml"], // クロール対象外のページ
+
   // 優先度と更新頻度の設定（オプション）
   changefreq: "weekly",
   priority: 0.7,
